0aa0f25ee005c46872357ad9144c2feda6a8b3dd
ryancdavey1 <ryancdavey1@gmail.com>
2020-11-16 22:51:11 -0700

    commit tree c05c939e54e8eb4447b5e0233a5901c22ea57e3c

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+document.addEventListener("DOMContentLoaded", () => {
+  return fetch(imgUrl)
+    .then(resp => resp.json())
+    .then(json => console.log(json))
+});
+
+function renderImages(images) {
+  const c = document.getElementById("dog-image-container")
+  images.forEach(image => {
+    const img = document.createElement('img')
+    img.src = image
+    c.appendChild(img)
+  })
+}
